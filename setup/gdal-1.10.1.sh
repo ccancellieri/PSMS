c75302f61d0bb132c3b8d076dbe3a1ed985266a3
carlo cancellieri <ccancellieri@hotmail.com>
2013-11-08 15:41:49 +0100

    include postgresql support in ogr2ogr

--- setup/gdal-1.10.1.sh
+++ setup/gdal-1.10.1.sh
@@ -1,6 +1,7 @@
+ yum install expat-devel.i686
 wget http://download.osgeo.org/gdal/1.10.1/gdal1101.zip
 unzip gdal1101.zip
 
-LTFLAGS=--tag=gpp ./configure
+LTFLAGS=--tag=gpp ./configure --with-pg=/usr/pgsql-9.3/bin/pg_config
 make -j8
 make install
