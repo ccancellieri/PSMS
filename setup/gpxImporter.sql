bb1c4b2f0873cbb69c6462cabd6298bf229359de
carlo cancellieri <ccancellieri@hotmail.com>
2013-11-12 08:37:26 +0100

    add fid and some fix to gpxImporter.sql function

--- setup/gpxImporter.sql
+++ setup/gpxImporter.sql
@@ -16,74 +16,84 @@ EXCEPTION
 	return false;
 END;
 
-BEGIN
-	SELECT count(*) INTO isArea FROM pg_class WHERE relname='area' and relkind='r';
-	IF (isArea) THEN
-		-- INSERT NEW ROWS
-		INSERT INTO area (area_id, hectares,wkb_geometry)
-		SELECT name, st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,wkb_geometry
-			FROM tracks
-			WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+$'
-				AND
-				name NOT IN (SELECT area_id FROM area);
-		-- UPDATE EXISTING ROWS
-		UPDATE area SET
-			area_id = tracks.name,
-			hectares = st_area(ST_Transform(ST_ConvexHull(tracks.wkb_geometry),3857))/10000,
-			wkb_geometry = tracks.wkb_geometry
+SELECT count(*) INTO isArea FROM pg_class WHERE relname='area' and relkind='r';
+IF (isArea) THEN
+	-- INSERT NEW ROWS
+	INSERT INTO area (fid, area_id, hectares,wkb_geometry)
+	SELECT nextval('fid_area'), name, st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,ST_ConvexHull(wkb_geometry)
 		FROM tracks
-		WHERE tracks.name=area_id;
-		
-	END IF;
-EXCEPTION
-	WHEN OTHERS THEN
+		WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+$'
+			AND
+			name NOT IN (SELECT area_id FROM area);
+	-- UPDATE EXISTING ROWS
+	UPDATE area SET
+		fid = area.fid,
+		area_id = tracks.name,
+		hectares = st_area(ST_Transform(ST_ConvexHull(tracks.wkb_geometry),3857))/10000,
+		wkb_geometry = ST_ConvexHull(tracks.wkb_geometry)
+	FROM tracks
+	WHERE tracks.name=area_id;
+ELSE
 	RAISE NOTICE 'Table "area" does not exists: creating it.';
+
+	CREATE SEQUENCE fid_area
+	  INCREMENT 1
+	  MINVALUE 1
+	  MAXVALUE 9223372036854775807
+	  START 1
+	  CACHE 1;  
+	
 	--DROP TABLE IF EXISTS area CASCADE;
-	CREATE TABLE area (area_id, hectares,wkb_geometry)
+	CREATE TABLE area (fid, area_id, hectares,wkb_geometry)
 	AS
-	(SELECT name, st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,wkb_geometry FROM tracks WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+$');
+	(SELECT nextval('fid_area'), name, st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,ST_ConvexHull(wkb_geometry) FROM tracks WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+$');
 
 	ALTER TABLE area
-	  ADD CONSTRAINT area_id PRIMARY KEY (area_id);
+	  ADD CONSTRAINT area_id PRIMARY KEY (fid);
 	  
 	CREATE INDEX area_geom_idx
 	  ON area USING gist (wkb_geometry);
 	  
 	CREATE INDEX area_idx
 	   ON area USING hash (area_id varchar_ops);
-END;
 
-BEGIN
-	SELECT count(*) INTO isPlot FROM pg_class WHERE relname='plot' and relkind='r';
-	IF (isPlot) THEN
-		-- INSERT NEW ROWS
-		INSERT INTO plot (area_id, plot_id, hectares,wkb_geometry)
-		SELECT SUBSTRING(name, '^.{3,3}-[0-9]+-[0-9]+-[0-9]+'), name , st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,wkb_geometry
-			FROM tracks 
-			WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+-[0-9]+$'
-				AND
-				name NOT IN (SELECT plot_id FROM plot);
-		-- UPDATE EXISTING ROWS
-		UPDATE plot SET
-			area_id = SUBSTRING(tracks.name, '^.{3,3}-[0-9]+-[0-9]+-[0-9]+'),
-			plot_id = tracks.name,
-			hectares = st_area(ST_Transform(ST_ConvexHull(tracks.wkb_geometry),3857))/10000,
-			wkb_geometry = tracks.wkb_geometry
-		FROM tracks
-		WHERE tracks.name=plot_id;
-	END IF;
-	
-EXCEPTION
-	WHEN OTHERS THEN
+END IF;
+
+SELECT count(*) INTO isPlot FROM pg_class WHERE relname='plot' and relkind='r';
+IF (isPlot) THEN
+	-- INSERT NEW ROWS
+	INSERT INTO plot (fid, area_id, plot_id, hectares,wkb_geometry)
+	SELECT nextval('fid_plot'), SUBSTRING(name, '^.{3,3}-[0-9]+-[0-9]+-[0-9]+'), name , st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,ST_ConvexHull(wkb_geometry)
+		FROM tracks 
+		WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+-[0-9]+$'
+			AND
+			name NOT IN (SELECT plot_id FROM plot);
+	-- UPDATE EXISTING ROWS
+	UPDATE plot SET
+		fid = plot.fid,
+		area_id = SUBSTRING(tracks.name, '^.{3,3}-[0-9]+-[0-9]+-[0-9]+'),
+		plot_id = tracks.name,
+		hectares = st_area(ST_Transform(ST_ConvexHull(tracks.wkb_geometry),3857))/10000,
+		wkb_geometry = ST_ConvexHull(tracks.wkb_geometry)
+	FROM tracks
+	WHERE tracks.name=plot_id;
+ELSE
 	RAISE NOTICE 'Table "plot" does not exists: creating it.';
 
+	CREATE SEQUENCE fid_plot
+	  INCREMENT 1
+	  MINVALUE 1
+	  MAXVALUE 9223372036854775807
+	  START 1
+	  CACHE 1;
+
 	--DROP TABLE IF EXISTS plot CASCADE;
-	CREATE TABLE plot (area_id, plot_id, hectares,wkb_geometry)
+	CREATE TABLE plot (fid, area_id, plot_id, hectares,wkb_geometry)
 	AS
-	(SELECT SUBSTRING(name, '^.{3,3}-[0-9]+-[0-9]+-[0-9]+'), name , st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,wkb_geometry FROM tracks WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+-[0-9]+$');
+	(SELECT nextval('fid_plot'), SUBSTRING(name, '^.{3,3}-[0-9]+-[0-9]+-[0-9]+'), name , st_area(ST_Transform(ST_ConvexHull(wkb_geometry),3857))/10000,ST_ConvexHull(wkb_geometry) FROM tracks WHERE name ~ '^.{3,3}-[0-9]+-[0-9]+-[0-9]+-[0-9]+$');
 
 	ALTER TABLE plot
-	  ADD CONSTRAINT plot_id PRIMARY KEY (plot_id);
+	  ADD CONSTRAINT plot_id PRIMARY KEY (fid);
 	  
 	CREATE INDEX plot_geom_idx
 	  ON plot USING gist (wkb_geometry);
@@ -93,7 +103,10 @@ EXCEPTION
 	   
 	CREATE INDEX plot_idx
 	   ON plot USING hash (plot_id varchar_ops);
-END;
+
+	
+END IF;
+	
 
 BEGIN
 	IF (EXISTS (SELECT area_id FROM summary LIMIT 1)) THEN
@@ -103,10 +116,10 @@ EXCEPTION
 	RAISE NOTICE 'View "summary" does not exists: creating it.';
 	CREATE OR REPLACE VIEW summary
 		AS
-		SELECT a.area_id, a.hectares, sum(p.hectares) AS plot_sum, (a.hectares-sum(p.hectares)) AS difference
-		-- OVER (PARTITION BY a.area_id) AS plot_sum
-		FROM area a join plot p ON a.area_id=p.area_id
-		GROUP BY (a.area_id);
+		SELECT a.area_id, a.hectares, sum(p.hectares) OVER (PARTITION BY a.area_id) AS plot_sum, (a.hectares-(sum(p.hectares) OVER (PARTITION BY a.area_id))) AS difference
+		
+		FROM area a join plot p ON a.area_id=p.area_id;
+		-- GROUP BY (a.area_id);
 END;
 
 return true;
